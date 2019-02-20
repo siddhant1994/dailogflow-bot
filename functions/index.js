2d64e3bfbe5e0b210076301190f59a20d13f4945
siddhant1994 <siddhant.s@sportzinteractive.net>
2019-02-20 15:58:26 +0530

    Testing

--- functions/index.js
+++ functions/index.js
@@ -10,7 +10,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+//test
 'use strict';
 
 // Import the Dialogflow module from the Actions on Google client library.
